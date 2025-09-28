fd8c4a3aa7b77f037ca33c4f44489f593f3cee22
Dilara <107860010+dilayre@users.noreply.github.com>
2025-09-28 21:48:38 +0400

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -11,9 +11,10 @@ else:
 # keep checking until we find a common multiple
 while True:
     if (greater % a == 0) and (greater % b == 0):
-        lcm = greater   # found the LCM
+        lcm = greater   # found the "Least Common Multiple"
         break           # stop the loop
     greater += 1        # try the next number
 
 # show the result
 print("The LCM of", a, "and", b, "is:", lcm)
+
