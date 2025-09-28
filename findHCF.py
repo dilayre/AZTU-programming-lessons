1b00dbabce47b15bd32d74dd1e399c7d9bacbd53
Dilara <107860010+dilayre@users.noreply.github.com>
2025-09-28 21:49:41 +0400

    Update findHCF.py

--- findHCF.py
+++ findHCF.py
@@ -11,7 +11,8 @@ else:
 # check which number divides both
 for i in range(1, smaller + 1):
     if (no1 % i == 0) and (no2 % i == 0):
-        hcf = i   # keep updating hcf when condition is true
+        hcf = i   # keep updating "Highest Common Factor" when condition is true
 
 # print the result
 print("The HCF of", no1, "and", no2, "is:", hcf)
+
